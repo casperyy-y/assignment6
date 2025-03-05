77be9848cc77497e586a9517658a4b6450e77e99
casperyy-y <hl96770@mail.harpercollege.edu>
2025-03-06 07:18:26 +0800

    Create PS6P2.py

---
+++ PS6P2.py
@@ -0,0 +1,9 @@
+start = int(input("Enter start value: "))
+stop = int(input("Enter stop value: "))
+step = int(input("Enter increment value: "))
+
+num = start
+
+while num <= stop:
+    print(num)
+    num += step  # Increase by step value
